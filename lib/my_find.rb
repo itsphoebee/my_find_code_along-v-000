77132a8dce33166340e36833d724661cf59d9aa4
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 04:36:57 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,9 @@ def my_find(collection)
   i=0
   while i < collection.length
     yield(collection[i])
-    if yield(collection[i]) == true
-      return collection[i]
-    i += 1
+      if yield(collection[i]) == true
+        return collection[i]
+      end
+      i += 1
   end
-end
 end
