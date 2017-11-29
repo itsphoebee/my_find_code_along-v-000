894cf8cd836724ea93b854e1e05210564aaaea80
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 04:35:28 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def my_find(collection)
   i=0
-  new_collection = []
   while i < collection.length
-    new_collection << if yield(collection[i]) == true
+    yield(collection[i])
+    if yield(collection[i]) == true
+      return collection[i]
     i += 1
   end
-  end
-  return new_collection
+
 end
