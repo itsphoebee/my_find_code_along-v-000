568a31c429d007dbc0c204513fe533d1229f841c
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 04:26:21 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,16 @@ require 'pry'
 
 def my_find(collection)
   i=0
+  new_collection = []
   while i < collection.length
-    yield(collection[i])
+    new_collection << yield(collection[i])
     i += 1
   end
+  
+  if new_collection.length > 0
+    return new_collection
+  else
+    nil
+  end
 end
+
